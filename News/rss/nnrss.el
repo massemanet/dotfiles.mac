a88c1b4ef5e40773daf821d7174700fe11a1486b
mats cronqvist <masse@klarna.com>
2011-12-08 11:54:31 +0100

    found more old crap

--- News/rss/nnrss.el
+++
@@ -1,3 +0,0 @@
-;; -*- coding: emacs-mule; -*-
-(setq nnrss-group-alist '(("orrenius" "http://blogg.expressen.se/orrenius/rss.jsp" "orrenius") ("Robert Lauls blogg" "http://blogg.aftonbladet.se/6/index.rss" "")))
-(setq nnrss-server-data '(("orrenius" 59) ("Robert Lauls blogg" 232)))
