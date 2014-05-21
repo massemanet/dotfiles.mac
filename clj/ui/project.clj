60763c968f1d2ed64db90b5ef9a121f041a08a4d
mats cronqvist <masse@klarna.com>
2014-05-21 13:42:31 +0200

    trash old files

--- clj/ui/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject ui "0.0.1"
-  :description "FIXME: playin' around"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [seesaw "1.2.2"]])
--- clj/ui/test/ui/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns ui.test.core
-  (:use [ui.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
