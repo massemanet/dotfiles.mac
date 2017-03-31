d1e2a98c3f9e841d648885af1878a5217eedd663
mats cronqvist <masse@cronqvi.st>
2017-03-31 15:03:48 +0200

    minor flycheck tweak

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -104,6 +104,7 @@
     :predicate (lambda ()
                  (string-suffix-p ".erl" (buffer-file-name))))
   (setq flycheck-erlang-include-path '("../include"))
+  (setq flycheck-erlang-library-path '("../_build/default/lib/*/ebin"))
   (setq safe-local-variable-values
         (quote ((allout-layout . t)
                 (erlang-indent-level . 4)
@@ -265,4 +266,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(edts-xref-checks nil)
  '(purescript-mode-hook (quote (turn-on-purescript-indentation))))
