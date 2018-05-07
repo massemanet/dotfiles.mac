393ed4a3d4c80108df07c2a0ba4ddd4847611da4
mats cronqvist <masse@cronqvi.st>
2018-05-07 16:46:26 +0200

    added some packages to cask

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -206,7 +206,7 @@ Repeated invocations toggle between the two most recently open buffers."
     ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (dockerfile-mode flycheck-julia julia-mode julia-repl color-theme-sanityinc-solarized yaml-mode solarized-theme smart-mode-line purescript-mode pallet nyan-mode markdown-mode magit json-mode js2-mode highlight-parentheses gruvbox-theme go-mode flymake-jshint flycheck-rebar3 flycheck-elixir flycheck-demjsonlint exec-path-from-shell eproject edts alchemist)))
+    (kubernetes cargo flycheck-rust flymake-rust rust-mode rust-playground dockerfile-mode flycheck-julia julia-mode julia-repl color-theme-sanityinc-solarized yaml-mode solarized-theme smart-mode-line purescript-mode pallet nyan-mode markdown-mode magit json-mode js2-mode highlight-parentheses gruvbox-theme go-mode flymake-jshint flycheck-rebar3 flycheck-elixir flycheck-demjsonlint exec-path-from-shell eproject edts alchemist)))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
