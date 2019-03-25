de14014a3688fdd7423cd398f2dcef0d49f2e082
mats cronqvist <masse@cronqvi.st>
2019-03-25 20:35:45 +0100

    learning bash completions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -193,6 +193,9 @@ Repeated invocations toggle between the two most recently open buffers."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(electric-indent-mode nil)
+ '(package-selected-packages
+   (quote
+    (flycheck-kotlin jdee kotlin-mode meghanada yaml-mode solarized-theme smart-mode-line rust-playground purescript-mode pallet nyan-mode kubernetes julia-repl julia-mode json-mode js2-mode highlight-parentheses gruvbox-theme go-mode flymake-rust flymake-jshint flycheck-rust flycheck-rebar3 flycheck-julia flycheck-elixir flycheck-demjsonlint exec-path-from-shell erlang eproject dockerfile-mode company cargo)))
  '(term-default-bg-color nil)
  '(term-default-fg-color nil))
 
